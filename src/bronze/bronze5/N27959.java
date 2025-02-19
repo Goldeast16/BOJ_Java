67802e1e06afb2ce3630872ee3484a312526f27f
Donggeon <goldeastfounder@gmail.com>
2025-02-19 09:04:56 +0900

    20250219

--- src/bronze/bronze5/N27959.java
+++ src/bronze/bronze5/N27959.java
@@ -1,4 +1,19 @@
 package bronze.bronze5;
 
+import java.util.Scanner;
+
 public class N27959 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt() * 100;
+        int price = sc.nextInt();
+
+        if (price <= N) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
 }
