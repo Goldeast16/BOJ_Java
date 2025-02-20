4b16a78a299e24674ef81ecb551f086b3c93d5b4
Donggeon <goldeastfounder@gmail.com>
2025-02-20 09:34:31 +0900

    20250220

--- src/bronze/bronze4/N13136.java
+++ src/bronze/bronze4/N13136.java
@@ -1,7 +1,30 @@
 package bronze.bronze4;
 
+import java.util.Scanner;
+
 public class N13136 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int R = sc.nextInt();
+        int C = sc.nextInt();
+        int N = sc.nextInt();
+        long A;
+        long B;
+
+        if (R % N != 0) {
+            A = (long) R / N + 1;
+        } else {
+            A = (long) R / N;
+        }
+
+        if (C % N != 0) {
+            B = (long) C / N + 1;
+        } else {
+            B = (long) C / N;
+        }
+
+        System.out.println(A*B);
 
     }
 }
