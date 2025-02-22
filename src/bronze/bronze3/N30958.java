3606d35a1badd354c2fc4b6757f3969522eae0b6
Donggeon <goldeastfounder@gmail.com>
2025-02-22 09:46:22 +0900

    20250222

--- src/bronze/bronze3/N30958.java
+++ src/bronze/bronze3/N30958.java
@@ -1,6 +1,7 @@
 package bronze.bronze3;
 // Stream 문법 배움
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -8,6 +9,36 @@ import java.util.stream.Collectors;
 public class N30958 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+        String input = sc.nextLine();
+        Map<String, Integer> map = new HashMap<>();
+        int maxValue = 0;
+
+        for (int i = 0; i < N; i++) {
+            char ch = input.charAt(i);
+
+            if(Character.isAlphabetic(ch)) {
+                String key = String.valueOf(ch);
+
+                if (map.containsKey(key)) {
+                    map.put(key, map.get(key) + 1);
+                } else {
+                    map.put(key, 1);
+                }
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxValue) {
+                maxValue = entry.getValue();
+            }
+        }
+
+        System.out.println(maxValue);
+
+        /*
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         scanner.nextLine();
@@ -34,5 +65,6 @@ public class N30958 {
                 // 만약 없으면 0 반환
 
         System.out.println(maxFrequency);
+         */
     }
 }
