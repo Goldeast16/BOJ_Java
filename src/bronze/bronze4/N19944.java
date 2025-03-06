dff233bb257914919d5d757fdb81dc7f8d18ac57
Donggeon <goldeastfounder@gmail.com>
2025-03-06 09:11:17 +0900

    20250306

--- src/bronze/bronze4/N19944.java
+++ src/bronze/bronze4/N19944.java
@@ -1,4 +1,29 @@
 package bronze.bronze4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class N19944 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        tleTTakClassifier(n, m);
+    }
+
+    static void tleTTakClassifier(int n, int m) {
+        if (m == 1 || m == 2) {
+            System.out.println("NEWBIE!");
+        } else if (m <= n) {
+            System.out.println("OLDBIE!");
+        } else {
+            System.out.println("TLE!");
+        }
+    }
 }
+
+
