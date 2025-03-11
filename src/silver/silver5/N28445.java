b99969662e4248181c392f87a1f1bc8520a396f6
Donggeon <goldeastfounder@gmail.com>
2025-03-11 15:39:51 +0900

    202503011

--- src/silver/silver5/N28445.java
+++ src/silver/silver5/N28445.java
@@ -1,4 +1,45 @@
 package silver.silver5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class N28445 {
+
+    public static void main(String[] args) throws IOException {
+        String[] parents = new String[4];
+        String[][] children = new String[16][2];
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer father = new StringTokenizer(br.readLine());
+        parents[0] = father.nextToken();
+        parents[1] = father.nextToken();
+        StringTokenizer mother = new StringTokenizer(br.readLine());
+        parents[2] = mother.nextToken();
+        parents[3] = mother.nextToken();
+
+        String[] sortedParents= Arrays.stream(parents).
+                distinct().
+                sorted().
+                toArray(String[]::new);
+
+        int howManyColors = sortedParents.length;
+
+        for (int i = 0; i < howManyColors; i++) {
+            for (int j = 0; j < howManyColors; j++) {
+                children[howManyColors*i+j][0] = sortedParents[i];
+                children[howManyColors*i+j][1] = sortedParents[j];
+            }
+        }
+
+        for (String[] child : children) {
+            if (child[0] == null) {
+                break;
+            }
+            System.out.print(child[0] + " ");
+            System.out.println(child[1]);
+        }
+    }
 }
