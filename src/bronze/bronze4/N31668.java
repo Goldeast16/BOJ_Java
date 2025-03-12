e1d0df9acce5eda7ed4c4bd114643158b4f8a92e
Donggeon <goldeastfounder@gmail.com>
2025-03-12 17:52:48 +0900

    20250312

--- src/bronze/bronze4/N31668.java
+++ src/bronze/bronze4/N31668.java
@@ -3,18 +3,17 @@ package bronze.bronze4;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class N31668 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int S = Integer.parseInt(st.nextToken());
-        int T = Integer.parseInt(st.nextToken());
-        int D = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
 
-        double time = (double) D / (S * 2);
-        System.out.println((int) time*T);
+        int howMany = M/N;
+        int change = howMany*K;
+        System.out.println(change);
 
     }
 }
