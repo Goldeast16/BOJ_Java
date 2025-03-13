85e9d5290b534804fe56b21bd98be2faffee02e4
Donggeon <goldeastfounder@gmail.com>
2025-03-13 09:15:48 +0900

    20250313

--- src/bronze/bronze3/N2754.java
+++ src/bronze/bronze3/N2754.java
@@ -1,7 +1,30 @@
 package bronze.bronze3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class N2754 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        HashMap<String, Double> score = new HashMap<>();
+        score.put("A+", 4.3);
+        score.put("A0", 4.0);
+        score.put("A-", 3.7);
+        score.put("B+", 3.3);
+        score.put("B0", 3.0);
+        score.put("B-", 2.7);
+        score.put("C+", 2.3);
+        score.put("C0", 2.0);
+        score.put("C-", 1.7);
+        score.put("D+", 1.3);
+        score.put("D0", 1.0);
+        score.put("D-", 0.7);
+        score.put("F", 0.0);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(score.get(s));
 
     }
 }
