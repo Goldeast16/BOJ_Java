3708ee428563d6ed29a5191d1152d3715b986fd0
Donggeon <goldeastfounder@gmail.com>
2025-03-16 20:37:57 +0900

    20250316

--- src/bronze/bronze1/N11179.java
+++ src/bronze/bronze1/N11179.java
@@ -1,4 +1,41 @@
 package bronze.bronze1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class N11179 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int decimal = Integer.parseInt(br.readLine());
+        String binary = decimalToBinary(decimal);
+        binary = reverseBinary(binary);
+        int reDecimal = binaryToDecimal(binary);
+        System.out.println(reDecimal);
+    }
+
+    public static String decimalToBinary(int decimal) {
+        if (decimal == 0) {
+            return "0";
+        }
+
+        StringBuilder binary = new StringBuilder();
+        while (decimal > 0) {
+            binary.insert(0, decimal % 2);
+            decimal /= 2;
+        }
+        return binary.toString();
+    }
+
+    public static int binaryToDecimal(String binary) {
+        int decimal = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            decimal = decimal * 2 + (binary.charAt(i) - '0');
+        }
+        return decimal;
+    }
+
+    public static String reverseBinary (String binary) {
+        return new StringBuilder(binary).reverse().toString();
+    }
 }
