1830e29e05b1caeee186c957add3796e23330be8
Donggeon <goldeastfounder@gmail.com>
2025-03-17 21:53:01 +0900

    20250317

--- src/bronze/bronze1/N30503.java
+++ src/bronze/bronze1/N30503.java
@@ -17,6 +17,32 @@ public class N30503 {
         int Q = Integer.parseInt(br.readLine());
         for (int i = 0; i < Q; i++) {
             StringTokenizer Query = new StringTokenizer(br.readLine());
+            int isCheck = Integer.parseInt(Query.nextToken());
+            int start = Integer.parseInt(Query.nextToken());
+            int end = Integer.parseInt(Query.nextToken());
+            if (isCheck == 1) {
+                int species = Integer.parseInt(Query.nextToken());
+                checkQuery(field, start, end, species);
+            } else {
+                field = deleteQuery(field, start, end);
+            }
         }
+    }
+
+    public static int[] deleteQuery(int[] field, int start, int end) {
+        for (int i = start - 1; i < end; i++) {
+            field[i] = 0;
+        }
+        return field;
+    }
+
+    public static void checkQuery(int[] field, int start, int end, int species) {
+        int count = 0;
+        for (int i = start - 1; i < end; i++) {
+            if (field[i] == species) {
+                count++;
+            }
+        }
+        System.out.println(count);
     }
 }
