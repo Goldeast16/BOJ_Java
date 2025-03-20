82f05e6c067f29342ae243b3b56c1367b9df54b9
Donggeon <goldeastfounder@gmail.com>
2025-03-20 09:15:21 +0900

    20250320

--- src/bronze/bronze3/N28061.java
+++ src/bronze/bronze3/N28061.java
@@ -3,10 +3,22 @@ package bronze.bronze3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class N28061 {
 
     public static void main(String[] args) throws IOException {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer lemons = new StringTokenizer(br.readLine());
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            int lemon = Integer.parseInt(lemons.nextToken());
+            if (lemon - (N - i) > max) {
+                max = lemon - (N - i);
+            }
+        }
+        System.out.println(max);
     }
 }
