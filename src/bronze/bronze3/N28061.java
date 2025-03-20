07302cac47c649789ef1dafc28d56f2d2850b8da
Donggeon <goldeastfounder@gmail.com>
2025-03-20 09:07:26 +0900

    20250320

---
+++ src/bronze/bronze3/N28061.java
@@ -0,0 +1,12 @@
+package bronze.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N28061 {
+
+    public static void main(String[] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
