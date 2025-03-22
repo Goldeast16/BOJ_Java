1019775cc6ae942fd07c9d57a705dc4876420f80
Donggeon <goldeastfounder@gmail.com>
2025-03-22 10:12:24 +0900

    20250322

--- src/bronze/bronze2/N30684.java
+++ src/bronze/bronze2/N30684.java
@@ -1,9 +1,27 @@
 package bronze.bronze2;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class N30684 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String chairman = "ZZZ";
 
+        for (int i = 0; i < N; i++) {
+            String member = br.readLine();
+
+            if (member.length() != 3) {
+                continue;
+            }
+
+            if (member.compareTo(chairman) < 0) {
+                    chairman = member;
+            }
+
+        }
+        System.out.println(chairman);
     }
 }
