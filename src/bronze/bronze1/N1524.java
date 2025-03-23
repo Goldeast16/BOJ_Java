961ac3a624abe846b14b10e59d527c49af14381c
Donggeon <goldeastfounder@gmail.com>
2025-03-23 21:08:11 +0900

    20250323

--- src/bronze/bronze1/N1524.java
+++ src/bronze/bronze1/N1524.java
@@ -1,9 +1,43 @@
 package bronze.bronze1;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class N1524 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
+        for (int i = 0; i < T; i++) {
+            String empty = br.readLine();
+            int sMax = 0, bMax = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            StringTokenizer sj = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int now = Integer.parseInt(sj.nextToken());
+                if (sMax < now) {
+                    sMax = now;
+                }
+            }
+
+            StringTokenizer sb = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                int now = Integer.parseInt(sb.nextToken());
+                if (bMax < now) {
+                    bMax = now;
+                }
+            }
+
+            if (sMax < bMax) {
+                System.out.println("B");
+            } else {
+                System.out.println("S");
+            }
+        }
     }
 }
