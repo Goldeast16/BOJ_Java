3c072f15548a645dde3c4145624ff2f386297982
Donggeon <goldeastfounder@gmail.com>
2025-03-26 11:32:32 +0900

    20250326

--- src/bronze/bronze4/N4470.java
+++ src/bronze/bronze4/N4470.java
@@ -1,4 +1,20 @@
 package bronze.bronze4;
 
+import java.io.*;
+
 public class N4470 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] texts = new String[N];
+        for (int i = 0; i < N; i++) {
+            texts[i] = br.readLine();
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++) {
+            bw.write((i+1) + ". " + texts[i] + "\n");
+            bw.flush();
+        }
+    }
 }
