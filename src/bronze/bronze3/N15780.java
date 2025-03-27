74ac0b16108d9b03a8a126bc06a7f4c6d1eb54a5
Donggeon <goldeastfounder@gmail.com>
2025-03-27 09:11:34 +0900

    20250327

--- src/bronze/bronze3/N15780.java
+++ src/bronze/bronze3/N15780.java
@@ -1,10 +1,31 @@
 package bronze.bronze3;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class N15780 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer nAndK = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(nAndK.nextToken());
+        int K = Integer.parseInt(nAndK.nextToken());
+        int[] multi = new int[K];
+        String available = "NO";
 
+        StringTokenizer holes = new StringTokenizer(br.readLine());
+        for (int i = 0; i < K; i++) {
+            multi[i] = Integer.parseInt(holes.nextToken());
+        }
 
+        int count = 0;
+        for (int now : multi) {
+            count += now/2 + now%2;
+            if (count >= N) {
+                available = "YES";
+                break;
+            }
+        }
+
+        System.out.println(available);
     }
 }
