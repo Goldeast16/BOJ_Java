e6e360065f8eecde4f962e9892bbf427bd27c986
Donggeon <goldeastfounder@gmail.com>
2025-03-29 09:34:03 +0900

    20250329

--- src/bronze/bronze1/N17283.java
+++ src/bronze/bronze1/N17283.java
@@ -1,4 +1,23 @@
 package bronze.bronze1;
 
+import java.io.*;
+
 public class N17283 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        int R = Integer.parseInt(br.readLine());
+        int sumLength = 0;
+        int branchCount = 1;
+        L = L * R / 100;
+
+        while (L > 5) {
+            branchCount *= 2;
+            sumLength += L * branchCount;
+            L = L * R / 100;
+        }
+
+        System.out.println(sumLength);
+    }
 }
