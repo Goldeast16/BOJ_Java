5d7f01091f42030d54dcd8e63fb864b21993e4db
Donggeon <goldeastfounder@gmail.com>
2025-04-02 23:24:34 +0900

    20250402

--- src/bronze/bronze4/N17388.java
+++ src/bronze/bronze4/N17388.java
@@ -1,9 +1,28 @@
 package bronze.bronze4;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class N17388 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split("");
+        int S = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
+        int H = Integer.parseInt(input[2]);
 
+        int sum = S+K+H;
+        if (sum >= 100) {
+            System.out.println("OK");
+        } else {
+            if (S < K && S < H) {
+                System.out.println("Soongsil");
+            } else if (K < S && K < H) {
+                System.out.println("Korea");
+            } else {
+                System.out.println("Hanyang");
+            }
+        }
     }
 }
