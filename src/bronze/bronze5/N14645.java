4de26abbc6b5014ab6ad041529c4aa6aee9ba66f
Donggeon <goldeastfounder@gmail.com>
2025-04-02 23:18:21 +0900

    20250402

--- src/bronze/bronze5/N14645.java
+++ src/bronze/bronze5/N14645.java
@@ -1,9 +1,22 @@
 package bronze.bronze5;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class N14645 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int K = Integer.parseInt(input[1]);
 
+        for (int i = 0; i < N; i++) {
+            input = br.readLine().split(" ");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+        }
+
+        System.out.println("비와이");
     }
 }
