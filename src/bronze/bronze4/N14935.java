cc024746fa815d39b8a88c70d4a7e5b9a17d966a
Donggeon <goldeastfounder@gmail.com>
2025-04-03 21:55:41 +0900

    20250403

---
+++ src/bronze/bronze4/N14935.java
@@ -0,0 +1,19 @@
+package bronze.bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N14935 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split("");
+        int count = 0;
+        int before = 0;
+
+        while (true) {
+
+        }
+
+    }
+}
