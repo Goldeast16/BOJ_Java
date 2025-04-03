8aeb2d78ef57de2aade042bfcbb2b79a1dc88a0b
Donggeon <goldeastfounder@gmail.com>
2025-04-03 22:09:29 +0900

    20250403

--- src/bronze/bronze4/N14935.java
+++ src/bronze/bronze4/N14935.java
@@ -8,12 +8,28 @@ public class N14935 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split("");
+        System.out.println("FA");
+        /*
         int count = 0;
-        int before = 0;
+        int before = Integer.parseInt(input[0]) * input.length;
+        boolean isFA = false;
 
         while (true) {
-
+            System.out.println(before);
+            if (before / 10 == 0) {
+                isFA = true;
+                break;
+            }
+            String[] nowString = String.valueOf(before).split("");
+            int now = Integer.parseInt(nowString[0]) * nowString.length;
+            before = now;
         }
 
+        if (isFA) {
+            System.out.println("FA");
+        } else {
+            System.out.println("NFA");
+        }
+        */
     }
 }
