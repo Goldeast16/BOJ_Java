a57a8b9e2b1cf0b0d2f2d6301f35ed02e427c252
Donggeon <goldeastfounder@gmail.com>
2025-04-04 09:57:16 +0900

    20250404

--- src/bronze/bronze3/N4892.java
+++ src/bronze/bronze3/N4892.java
@@ -1,9 +1,29 @@
 package bronze.bronze3;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class N4892 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        int count = 0;
+        while (input != 0) {
+            count++;
+            System.out.print(count + ". ");
+            int nOne = 3 * input;
+            int nFour;
+            if (nOne % 2 == 0) {
+                System.out.print("even ");
+                nFour = input/2;
+            } else {
+                System.out.print("odd ");
+                nFour = (input - 1)/2;
+            }
+            System.out.print(nFour + "\n");
 
+            input = Integer.parseInt(br.readLine());
+        }
     }
 }
