8bc46586e1c1ab3ceb467632a99e6da5eb222229
Donggeon <goldeastfounder@gmail.com>
2025-04-07 10:48:59 +0900

    20250407

--- src/silver/silver5/N14730.java
+++ src/silver/silver5/N14730.java
@@ -1,4 +1,18 @@
 package silver.silver5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class N14730 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split(" ");
+            sum += Integer.parseInt(input[0]) * (Integer.parseInt(input[1]));
+        }
+        System.out.println(sum);
+    }
 }
