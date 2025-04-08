3d4db12e20ee73b54840d8c110f53c97d7be6946
Donggeon <goldeastfounder@gmail.com>
2025-04-08 09:53:38 +0900

    20250408

--- src/silver/silver4/N14594.java
+++ src/silver/silver4/N14594.java
@@ -1,10 +1,29 @@
 package silver.silver4;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class N14594 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] rooms = new int[N];
+        for (int i = 0; i < N; i++) {
+            rooms[i] = 1;
+        }
+        int M = Integer.parseInt(br.readLine());
+        for (int i = 0; i < M; i++) {
+            String[] xAndY = br.readLine().split(" ");
+            int x = Integer.parseInt(xAndY[0]);
+            int y = Integer.parseInt(xAndY[1]);
 
+            for (int j = x; j < y; j++) {
+                rooms[j] = 0;
+            }
+        }
+        System.out.println(Arrays.stream(rooms).sum());
 
     }
 }
