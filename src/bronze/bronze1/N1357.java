4620f67952cbd1a0a9b64f8e812069606fe6e812
Donggeon <goldeastfounder@gmail.com>
2025-04-12 16:12:53 +0900

    20250412

--- src/bronze/bronze1/N1357.java
+++ src/bronze/bronze1/N1357.java
@@ -1,4 +1,26 @@
 package bronze.bronze1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class N1357 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int X = Integer.parseInt(input[0]);
+        int Y = Integer.parseInt(input[1]);
+
+        System.out.println(Rev(Rev(X) + Rev(Y)));
+    }
+
+    public static int Rev(int N) {
+        StringBuilder rev = new StringBuilder();
+        while (N != 0) {
+            rev.append(N % 10);
+            N /= 10;
+        }
+        int revNum = Integer.parseInt(rev.toString());
+        return revNum;
+    }
 }
