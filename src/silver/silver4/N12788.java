a85a008547356edaf09ccf43f3dfb5b49f7b72dc
Donggeon <goldeastfounder@gmail.com>
2025-04-14 12:02:02 +0900

    20250414

---
+++ src/silver/silver4/N12788.java
@@ -0,0 +1,12 @@
+package silver.silver4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N12788 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
