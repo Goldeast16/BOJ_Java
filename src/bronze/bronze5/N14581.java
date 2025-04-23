71203f9b65c7fdddd8b5b9b76768c002d953b732
Donggeon <goldeastfounder@gmail.com>
2025-04-23 09:13:18 +0900

    20250423

---
+++ src/bronze/bronze5/N14581.java
@@ -0,0 +1,16 @@
+package bronze.bronze5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N14581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + name + "::fan:");
+        System.out.println(":fan::fan::fan:");
+
+    }
+}
