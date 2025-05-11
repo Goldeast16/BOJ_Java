6099b62ed8e2ec84f79d6edee003800213ed822b
Donggeon <goldeastfounder@gmail.com>
2025-05-11 13:09:07 +0900

    20250511

--- src/bronze/bronze3/N10250.java
+++ src/bronze/bronze3/N10250.java
@@ -14,9 +14,15 @@ public class N10250 {
             int H = Integer.parseInt(input[0]);
             int W = Integer.parseInt(input[1]);
             int N = Integer.parseInt(input[2]);
+            String room = "";
 
             String floor = String.valueOf((N-1)%H + 1);
-            String room = String.valueOf(N/H + 1);
+            if (N >= H) {
+                room = String.valueOf(N);
+            } else {
+                room = String.valueOf(N/H + 1);
+            }
+
             if (room.length() == 1) {
                 room = "0" + room;
             }
