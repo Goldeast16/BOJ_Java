c54b3c897686e23d26ad5adc98666dcf19cf8cb3
Donggeon <goldeastfounder@gmail.com>
2025-08-07 13:13:41 +0900

    DP 내일도 복습해야 함

--- src/silver/silver3/N1463.java
+++ src/silver/silver3/N1463.java
@@ -1,5 +1,34 @@
 package silver.silver3;
 
-public class N1463 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class N1463 {
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+
+        System.out.println(findShortCut(n) - 1);
+    }
+
+    static int findShortCut (int num) {
+        if (dp[num] == 0) {
+
+            if (num % 6 == 0) {
+                dp[num] = Math.min(findShortCut(num / 3), findShortCut(num / 2)) + 1;
+            } else if (num % 3 == 0) {
+                dp[num] =  Math.min(findShortCut(num / 3), findShortCut(num - 1)) + 1;
+            } else if (num % 2 == 0) {
+                dp[num] =  Math.min(findShortCut(num / 2), findShortCut(num - 1)) + 1;
+            } else {
+                dp[num] = findShortCut(num - 1) + 1;
+            }
+        }
+        return dp[num];
+    }
 }
