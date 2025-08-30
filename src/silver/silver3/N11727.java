0dd8f2fc2e8d6566e6d21a2dfeeee206694922d4
Donggeon <goldeastfounder@gmail.com>
2025-08-30 09:10:41 +0900

    250830

---
+++ src/silver/silver3/N11727.java
@@ -0,0 +1,23 @@
+package silver.silver3;
+
+import java.io.*;
+
+public class N11727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N  = Integer.parseInt(br.readLine());
+
+        int[] dp =  new int[Math.max(3, N + 1)];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        }
+
+        bw.write(dp[N] + "\n");
+        bw.flush();
+    }
+}
